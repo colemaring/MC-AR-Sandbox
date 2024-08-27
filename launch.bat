7510f59fc4ed178432da45aebb4c12c1632b39fc
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-08-27 11:43:48 -0400

    launch mc launcher

--- launch.bat
+++ launch.bat
@@ -2,4 +2,5 @@
 start /b /min "" cmd /c "runserver.bat"
 start /b /min "" cmd /c "node server.js"
 start /b /min "" cmd /c "python topoprojection.py"
+start /b /min "" "C:\XboxGames\Minecraft Launcher\Content\Minecraft.exe"
 exit /b 0
