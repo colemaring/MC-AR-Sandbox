6dde1e4aa148bec33559382c6a2c4203576bd1e4
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-08-28 10:36:08 -0400

    update launcher path for pc

--- launch.bat
+++ launch.bat
@@ -2,5 +2,5 @@
 start /b /min "" cmd /c "runserver.bat"
 start /b /min "" cmd /c "node server.js"
 start /b /min "" cmd /c "python topoprojection.py"
-start /b /min "" "C:\XboxGames\Minecraft Launcher\Content\Minecraft.exe"
+start /b /min "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Minecraft Launcher\Minecraft Launcher.lnk"
 exit /b 0
