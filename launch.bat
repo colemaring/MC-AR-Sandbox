ae55a2abaad680d5deffb64e2223f2badcacff7f
Cole Maring <65455664+colemaring@users.noreply.github.com>
2025-03-27 12:25:05 -0400

    Delete launch.bat

--- launch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start /b /min "" cmd /c "runserver.bat"
-start /b /min "" cmd /c "node server.js"
-start /b /min "" cmd /c "python topoprojection.py"
-start /b /min "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Minecraft Launcher\Minecraft Launcher.lnk"
-exit /b 0
